1dc7a5b5a27b5874abd88d5451aac1d7f402c03b
Ohm <ohm.g@powernewz.com>
2019-12-02 08:14:11 +0530

    space given after curly braces

--- app/api/server/v1/application.js
+++ app/api/server/v1/application.js
@@ -2,7 +2,7 @@ import { API } from '../api';
 
 API.v1.addRoute('getapplication', { authRequired: true }, {
 	get() {
-		const applications = {b:1};
+		const applications = { b:1 };
 		return API.v1.success({ applications });
 	},
 });
