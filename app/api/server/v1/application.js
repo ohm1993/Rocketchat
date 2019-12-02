095309deb365f8f1a3915b1e3996d74ba150d989
Ohm <ohm.g@powernewz.com>
2019-12-02 08:10:02 +0530

    space deleted

--- app/api/server/v1/application.js
+++ app/api/server/v1/application.js
@@ -2,21 +2,7 @@ import { API } from '../api';
 
 API.v1.addRoute('getapplication', { authRequired: true }, {
 	get() {
-		const applications =[{
-			'id':1,
-			'name':'CALENDAR',
-			'msg':'calender'
-			},
-			{
-			'id':2,
-			'name':'UTILITIES',
-			'msg':'utilities'
-			},
-			{
-			'id':3,
-			'name':'PERSONAL',
-			'msg':'personal'
-			}]
+		const applications = {b:1};
 		return API.v1.success({ applications });
 	},
 });
