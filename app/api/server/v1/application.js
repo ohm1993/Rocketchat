151556aa42beb1e7206e63c3c5519adb8278a54b
Ohm <ohm.g@powernewz.com>
2019-12-02 08:27:16 +0530

    applications api added

--- app/api/server/v1/application.js
+++ app/api/server/v1/application.js
@@ -2,7 +2,21 @@ import { API } from '../api';
 
 API.v1.addRoute('getapplication', { authRequired: true }, {
 	get() {
-		const applications = { b: 1 };
+		const applications = [{
+			id: 1,
+			name: 'CALENDAR',
+			msg: 'calender',
+		},
+		{
+			id: 2,
+			name: 'UTILITIES',
+			msg: 'utilities',
+		},
+		{
+			id: 3,
+			name: 'PERSONAL',
+			msg: 'personal',
+		}];
 		return API.v1.success({ applications });
 	},
 });
